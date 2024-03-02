8af6811cc9812817dbff4f7628a4996f4d2b31e9
AravindBabu003 <104773198+AravindBabu003@users.noreply.github.com>
2024-03-02 11:48:30 +0530

    Create Challenge3.java
    Partial solution for challenge 3

---
+++ Challenge3.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Test case 1
+        int[][] flights1 = {{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}};
+        System.out.println("Actual: " + findCheapestPrice(4, flights1, 0, 3, 1) + ", Expected: 700");
+
+        // Test case 2
+        int[][] flights2 = {{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}};
+        System.out.println("Actual: " + findCheapestPrice(4, flights2, 0, 3, 0) + ", Expected: -1");
+
+        // Test case 3
+        int[][] flights3 = {{0, 1, 100}, {1, 2, 100}, {0, 2, 500}};
+        System.out.println("Actual: " + findCheapestPrice(3, flights3, 0, 2, 0) + ", Expected: 500");
+    }
+
+    public static int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        // add your solution here
+        int cost = -1;
+        int flightRouteListSize = flights.length;
+        for(int i=0; i<=flightRouteListSize; i++){
+            if(dst>src) {
+                if (flights[i][0] == src && flights[i][1] == dst) {
+                    cost = flights[i][2];
+                }
+            }
+        }
+        return cost;
+    }
+}
